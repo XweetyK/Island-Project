94feeedc6ab5d864453cdf282f19a6db3c0a4e91
Leo Perugini <xweetyk@gmail.com>
2019-04-14 18:14:35 -0300

    Combat WIP
    the eye of the tiger.mp3

--- Island Project/Assets/Scripts/CombatInput.cs
+++ Island Project/Assets/Scripts/CombatInput.cs
@@ -4,12 +4,138 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class CombatInput : MonoBehaviour {
+	
+	enum InputCombo{NONE, ATTACK, SPECIAL, DEFENSE, RUN, ITEM};
 
+	[SerializeField] int _level;
+	[SerializeField] private float _limitTime;
 
-	void Start () {
-	}
+	private int[] _attackCombo = new int[10]{ 1, 1, 2, 1, 3, 4, 3, 4, 2, 2 };
+	private int[] _specialCombo = new int[10]{ 3, 3, 4, 2, 2, 1, 3, 1, 3, 4 };
+	private int[] _defenseCombo = new int[10]{ 4, 4, 1, 1, 4, 4, 3, 3, 2, 2 };
+	private int[] _runCombo = new int[3]{ 2, 2, 2 };
+	private int[] _itemCombo = new int[3]{ 5, 5, 5 };
+	private int[] _actualCombo = new int[10]{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+	private int _cont = 0;
+	private bool _blocked =false;
+	private int _levelLimit;
+	InputCombo _iCombo = InputCombo.NONE;
+	private bool _miss = false;
+	private bool _endTurn = false;
+	float _timer;
 
 	void Update () {
+		InputCheck ();
+		ComboCheck ();
+		Timer ();
+		if (_timer >= _limitTime) {
+			EndTurn ();
+		}
+	}
+
+	void InputCheck() {
+
+		if (_cont == _levelLimit) {
+			_blocked = true;
+		}
+		if(_blocked==false){
+			if (Input.GetButtonDown ("Up")) {
+				_actualCombo [_cont] = 1;
+			}
+			if (Input.GetButtonDown ("Down")) {
+				_actualCombo [_cont] = 2;
+			}
+			if (Input.GetButtonDown ("Left")) {
+				_actualCombo [_cont] = 3;
+			}
+			if (Input.GetButtonDown ("Right")) {
+				_actualCombo [_cont] = 4;
+			}
+		}		
+
+		if (Input.GetButtonDown("Space")) {
+			Send ();
+		}
+	}
+
+	void ComboCheck(){
+		switch (_actualCombo[0]) {
+		case 1:
+			_iCombo = InputCombo.ATTACK;
+			break;
+		case 3:
+			_iCombo = InputCombo.SPECIAL;
+			break;
+		case 4:
+			_iCombo = InputCombo.DEFENSE;
+			break;
+		case 2:
+			_iCombo = InputCombo.RUN;
+			break;
+		case 5:
+			_iCombo = InputCombo.ITEM;
+			break;
+		}
+
+		switch (_iCombo) {
+		case InputCombo.ATTACK:
+			if (_actualCombo [_cont] == _attackCombo [_cont]) {
+				_cont++;
+			} else {
+				_miss = true;
+			}
+			break;
+		case InputCombo.SPECIAL:
+			if (_actualCombo [_cont] == _specialCombo [_cont]) {
+				_cont++;
+			} else {
+				_miss = true;
+			}
+			break;
+		case InputCombo.DEFENSE:
+			if (_actualCombo [_cont] == _defenseCombo [_cont]) {
+				_cont++;
+			} else {
+				_miss = true;
+			}
+			break;
+		case InputCombo.RUN:
+			if (_actualCombo [_cont] == _runCombo [_cont]) {
+				_cont++;
+			} else {
+				_miss = true;
+			}
+			break;
+		case InputCombo.ITEM:
+			if (_actualCombo [_cont] == _itemCombo [_cont]) {
+				_cont++;
+			} else {
+				_miss = true;
+			}
+			break;
+		}
+	}
+
+	void Missed(){
+		if (_miss==true) {
+			_cont = 0;
+			for (int i = 0; i < 10; i++) {
+				_actualCombo [i] = 0;
+			}
+			_miss = false;
+		}
+	}
+
+	void Timer(){
+		_timer += 1 * Time.deltaTime;	
+	}
+
+	void EndTurn(){
+		_endTurn = true;
+	}
+
+	void Send(){
 		
 	}
 }
--- Island Project/Assets/Scripts/PlayerMov.cs
+++ Island Project/Assets/Scripts/PlayerMov.cs
@@ -3,72 +3,99 @@ using System.Collections;
 
 public class PlayerMov : MonoBehaviour {
 
-	public float walkSpeed = 2;
-	public float runSpeed = 6;
+	[SerializeField]private float _walkSpeed;
+	[SerializeField]private float _runSpeed;
+	[SerializeField]private float _turnSmoothTime;
+	[SerializeField]private float _speedSmoothTime;
+	[SerializeField]private float _animationWaitTime;
 
-	public float turnSmoothTime = 0.2f;
-	float turnSmoothVelocity;
+	float _turnSmoothVel;
+	float _speedSmoothVel;
+	float _currentSpeed;
 
-	public float speedSmoothTime = 0.1f;
-	float speedSmoothVelocity;
-	float currentSpeed;
+	bool _isRunning;
+	bool _isWalking;
+	bool _isSit;
+	bool _isCrouch;
 
-	bool isRunning;
-	bool isWalking;
-	bool isSit;
-	bool isCrouch;
+	float _timer = 0;
+	bool _counting = false;
 
-	Animator animator;
+	Animator _animator;
 
 	void Start () {
-		isWalking = isRunning = isCrouch = isSit = false;
-		animator = GetComponent<Animator> ();
+		_isWalking = _isRunning = _isCrouch = _isSit = false;
+		_animator = GetComponent<Animator> ();
 	}
 
 	void Update () {
 		KeyInput ();
-		Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
-		Vector2 inputDir = input.normalized;
-
-		if (inputDir != Vector2.zero) {
-			float targetRotation = Mathf.Atan2 (inputDir.x, inputDir.y) * Mathf.Rad2Deg;
-			transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
-		}
-			
-		float targetSpeed = ((isRunning) ? runSpeed : walkSpeed) * inputDir.magnitude;
-		currentSpeed = Mathf.SmoothDamp (currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
-
-		transform.Translate (transform.forward * currentSpeed * Time.deltaTime, Space.World);
-
+		Movement ();
 		UpdateAnimator ();
+		AnimTimer ();
+		Debug.Log (_timer);
+	}
+
+	void Movement(){
+		if (_counting==false || _isSit==true) {
+			Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+			Vector2 inputDir = input.normalized;
+
+			if (inputDir != Vector2.zero) {
+				float targetRotation = Mathf.Atan2 (inputDir.x, inputDir.y) * Mathf.Rad2Deg;
+				transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref _turnSmoothVel, _turnSmoothTime);
+			}
+
+			float targetSpeed = ((_isRunning) ? _runSpeed : _walkSpeed) * inputDir.magnitude;
+			_currentSpeed = Mathf.SmoothDamp (_currentSpeed, targetSpeed, ref _speedSmoothVel, _speedSmoothTime);
+			transform.Translate (transform.forward * _currentSpeed * Time.deltaTime, Space.World);
+		}
 	}
 
 	void KeyInput(){
 		if (Input.GetButton ("Space")) {
-			isRunning = true;
+			_isRunning = true;
 		} else {
-			isRunning = false;
+			_isRunning = false;
 		}
 		if (Input.GetButton ("Shift")) {
-			isCrouch = true;
+			_isCrouch = true;
 		} else {
-			isCrouch = false;
+			_isCrouch = false;
 		}
 		if (Input.GetAxis ("Horizontal") != 0 || Input.GetAxis ("Vertical") != 0) {
-			isWalking = true;
-			isSit = false;
+			_isWalking = true;
+			if (_isSit) {
+				_isSit = false;
+				_counting = true;
+			}
 		} else {
-			isWalking = false;
+			_isWalking = false;
 		}
 		if (Input.GetButtonDown ("One")) {
-			isSit = !isSit;
+			_isSit = true;
+			_counting = true;
 		}
 	}
 
 	void UpdateAnimator(){
-		animator.SetBool ("Walking", isWalking);
-		animator.SetBool ("Running", isRunning);
-		animator.SetBool ("Crouch", isCrouch);
-		animator.SetBool ("Sitting", isSit);
+		_animator.SetBool ("Walking", _isWalking);
+		_animator.SetBool ("Running", _isRunning);
+		_animator.SetBool ("Crouch", _isCrouch);
+		_animator.SetBool ("Sitting", _isSit);
+	}
+
+	void AnimTimer(){
+		switch (_counting) {
+		case true:
+			_timer += 1 * Time.deltaTime;
+			if (_timer >= _animationWaitTime) {
+				_counting = false;
+			}
+			break;
+		case false:
+			_timer = 0;
+			break;
+		}
 	}
 }
