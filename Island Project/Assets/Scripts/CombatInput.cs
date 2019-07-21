cbec6b198738f42c6d8ef54def0b05b0e3dc44dd
Leo Perugini <xwe>
2019-07-21 05:33:24 -0300

    Combat System and UI Animations

--- Island Project/Assets/Scripts/CombatInput.cs
+++ Island Project/Assets/Scripts/CombatInput.cs
@@ -4,138 +4,222 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class CombatInput : MonoBehaviour {
-	
-	enum InputCombo{NONE, ATTACK, SPECIAL, DEFENSE, RUN, ITEM};
 
-	[SerializeField] int _level;
-	[SerializeField] private float _limitTime;
+    enum InputCombo { NONE, ATTACK, SPECIAL, DEFENSE, RUN, ITEM };
 
-	private int[] _attackCombo = new int[10]{ 1, 1, 2, 1, 3, 4, 3, 4, 2, 2 };
-	private int[] _specialCombo = new int[10]{ 3, 3, 4, 2, 2, 1, 3, 1, 3, 4 };
-	private int[] _defenseCombo = new int[10]{ 4, 4, 1, 1, 4, 4, 3, 3, 2, 2 };
-	private int[] _runCombo = new int[3]{ 2, 2, 2 };
-	private int[] _itemCombo = new int[3]{ 5, 5, 5 };
-	private int[] _actualCombo = new int[10]{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    [SerializeField] int _level;
+    [SerializeField] private float _limitTime;
+    [SerializeField] private Sprite _arrowFilled;
+    [SerializeField] private Image[] _attackArrows;
+    [SerializeField] private Image[] _defenseArrows;
+    [SerializeField] private Image[] _specialArrows;
+    [SerializeField] [Range(0.0f, 1.0f)] private float _arrowOpacity;
+    [SerializeField] Animator _UiAnimator;
 
-	private int _cont = 0;
-	private bool _blocked =false;
-	private int _levelLimit;
-	InputCombo _iCombo = InputCombo.NONE;
-	private bool _miss = false;
-	private bool _endTurn = false;
-	float _timer;
+    private int[] _attackCombo = new int[10] { 1, 1, 2, 1, 3, 4, 3, 4, 2, 2 };
+    private int[] _specialCombo = new int[10] { 3, 3, 4, 2, 2, 1, 3, 1, 3, 4 };
+    private int[] _defenseCombo = new int[10] { 4, 4, 1, 1, 4, 4, 3, 3, 2, 2 };
+    //private int[] _runCombo = new int[3] { 2, 2, 2 };
+    //private int[] _itemCombo = new int[3] { 5, 5, 5 };
+    private int[] _actualCombo = new int[10] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
-	void Update () {
-		InputCheck ();
-		ComboCheck ();
-		Timer ();
-		if (_timer >= _limitTime) {
-			EndTurn ();
-		}
-	}
+    private int _cont = 0;
+    private bool _blocked = false;
+    private int _levelLimit;
+    InputCombo _iCombo = InputCombo.NONE;
+    private bool _miss = false;
+    private bool _endTurn = false;
+    float _timer;
 
-	void InputCheck() {
+    private Color _opacityBack;
+    private Color _opacityHeld;
 
-		if (_cont == _levelLimit) {
-			_blocked = true;
-		}
-		if(_blocked==false){
-			if (Input.GetButtonDown ("Up")) {
-				_actualCombo [_cont] = 1;
-			}
-			if (Input.GetButtonDown ("Down")) {
-				_actualCombo [_cont] = 2;
-			}
-			if (Input.GetButtonDown ("Left")) {
-				_actualCombo [_cont] = 3;
-			}
-			if (Input.GetButtonDown ("Right")) {
-				_actualCombo [_cont] = 4;
-			}
-		}		
+    private void Start() {
+        _opacityBack = _opacityHeld = Color.white;
+        _opacityBack.a = _arrowOpacity;
 
-		if (Input.GetButtonDown("Space")) {
-			Send ();
-		}
-	}
+        if (_level < 10) {
+            for (int i = _level; i < 10; i++) {
+                _attackArrows[i].gameObject.SetActive(false);
+                _defenseArrows[i].gameObject.SetActive(false);
+                _specialArrows[i].gameObject.SetActive(false);
+            }
+        }
 
-	void ComboCheck(){
-		switch (_actualCombo[0]) {
-		case 1:
-			_iCombo = InputCombo.ATTACK;
-			break;
-		case 3:
-			_iCombo = InputCombo.SPECIAL;
-			break;
-		case 4:
-			_iCombo = InputCombo.DEFENSE;
-			break;
-		case 2:
-			_iCombo = InputCombo.RUN;
-			break;
-		case 5:
-			_iCombo = InputCombo.ITEM;
-			break;
-		}
+        for (int i = 0; i < _level; i++) {
+            _attackArrows[i].color = _opacityBack;
+            _specialArrows[i].color = _opacityBack;
+            _defenseArrows[i].color = _opacityBack;
+        }
+    }
 
-		switch (_iCombo) {
-		case InputCombo.ATTACK:
-			if (_actualCombo [_cont] == _attackCombo [_cont]) {
-				_cont++;
-			} else {
-				_miss = true;
-			}
-			break;
-		case InputCombo.SPECIAL:
-			if (_actualCombo [_cont] == _specialCombo [_cont]) {
-				_cont++;
-			} else {
-				_miss = true;
-			}
-			break;
-		case InputCombo.DEFENSE:
-			if (_actualCombo [_cont] == _defenseCombo [_cont]) {
-				_cont++;
-			} else {
-				_miss = true;
-			}
-			break;
-		case InputCombo.RUN:
-			if (_actualCombo [_cont] == _runCombo [_cont]) {
-				_cont++;
-			} else {
-				_miss = true;
-			}
-			break;
-		case InputCombo.ITEM:
-			if (_actualCombo [_cont] == _itemCombo [_cont]) {
-				_cont++;
-			} else {
-				_miss = true;
-			}
-			break;
-		}
-	}
+    void Update() {
+        InputCheck();
+        //Timer ();
+        //if (_timer >= _limitTime) {
+        //	EndTurn ();
+        //}
+        Debug.Log(_cont);
+    }
 
-	void Missed(){
-		if (_miss==true) {
-			_cont = 0;
-			for (int i = 0; i < 10; i++) {
-				_actualCombo [i] = 0;
-			}
-			_miss = false;
-		}
-	}
+    void InputCheck() {
+        if (_cont == _level) {
+            _blocked = true;
+        }
+        if (_blocked == false) {
+            if (Input.GetButtonDown("Up")) {
+                _actualCombo[_cont] = 1;
+                ComboCheck();
+            }
+            if (Input.GetButtonDown("Down")) {
+                _actualCombo[_cont] = 2;
+                ComboCheck();
+            }
+            if (Input.GetButtonDown("Left")) {
+                _actualCombo[_cont] = 3;
+                ComboCheck();
+            }
+            if (Input.GetButtonDown("Right")) {
+                _actualCombo[_cont] = 4;
+                ComboCheck();
+            }
+        } else {
+            Send();
+        }
 
-	void Timer(){
-		_timer += 1 * Time.deltaTime;	
-	}
+        if (Input.GetButtonDown("Space")) {
+            Send();
+        }
+    }
 
-	void EndTurn(){
-		_endTurn = true;
-	}
+    void ComboCheck() {
+        switch (_actualCombo[0]) {
+            case 0:
+                _iCombo = InputCombo.NONE;
+                _UiAnimator.SetBool("Attack", false);
+                _UiAnimator.SetBool("Defense", false);
+                _UiAnimator.SetBool("Special", false);
+                break;
+            case 1:
+                _iCombo = InputCombo.ATTACK;
+                _UiAnimator.SetBool("Attack", true);
+                break;
+            case 2:
+                _iCombo = InputCombo.RUN;
+                break;
+            case 3:
+                _iCombo = InputCombo.SPECIAL;
+                _UiAnimator.SetBool("Special", true);
+                break;
+            case 4:
+                _iCombo = InputCombo.DEFENSE;
+                _UiAnimator.SetBool("Defense", true);
+                break;
+        }
+        switch (_iCombo) {
+            case InputCombo.NONE:
+                break;
+            case InputCombo.ATTACK:
+                if (_actualCombo[_cont] == _attackCombo[_cont]) {
+                    ArrowColorHeld();
+                    _cont++;
+                } else { Missed(); }
+                break;
+            case InputCombo.DEFENSE:
+                if (_actualCombo[_cont] == _defenseCombo[_cont]) {
+                    ArrowColorHeld();
+                    _cont++;
+                } else { Missed(); }
+                break;
+            case InputCombo.RUN:
+                if (_actualCombo[_cont] == _specialCombo[_cont]) {
+                    ArrowColorHeld();
+                    _cont++;
+                } else { Missed(); }
+                break;
+        }
+    }
 
-	void Send(){
-		
-	}
+    void Missed() {
+        _cont = 0;
+        Debug.Log("missed!");
+        ArrowColorMissed();
+        _UiAnimator.SetBool("Attack", false);
+        _UiAnimator.SetBool("Defense", false);
+        _UiAnimator.SetBool("Special", false);
+        _UiAnimator.SetBool("EndTurn", true);
+        for (int i = 0; i < 10; i++) {
+            _actualCombo[i] = 0;
+        }
+        _miss = false;
+    }
+
+    void Timer() {
+        _timer += Time.deltaTime;
+    }
+
+    void EndTurn() {
+        _endTurn = true;
+    }
+
+    void Send() {
+        ArrowSent();
+        _UiAnimator.SetBool("Attack", false);
+        _UiAnimator.SetBool("Defense", false);
+        _UiAnimator.SetBool("Special", false);
+        _UiAnimator.SetBool("EndTurn", true);
+        Debug.Log("send!");
+    }
+
+    void ArrowColorHeld() {
+        switch (_iCombo) {
+            case InputCombo.ATTACK:
+                _attackArrows[_cont].color = _opacityHeld;
+                break;
+            case InputCombo.SPECIAL:
+                _specialArrows[_cont].color = _opacityHeld;
+                break;
+            case InputCombo.DEFENSE:
+                _defenseArrows[_cont].color = _opacityHeld;
+                break;
+        }
+    }
+    void ArrowColorMissed() {
+        switch (_iCombo) {
+            case InputCombo.ATTACK:
+                for (int i = 0; i < _level; i++) {
+                    _attackArrows[i].color = _opacityBack;
+                }
+                break;
+            case InputCombo.SPECIAL:
+                for (int i = 0; i < _level; i++) {
+                    _specialArrows[i].color = _opacityBack;
+                }
+                break;
+            case InputCombo.DEFENSE:
+                for (int i = 0; i < _level; i++) {
+                    _defenseArrows[i].color = _opacityBack;
+                }
+                break;
+        }
+    }
+    void ArrowSent() {
+        switch (_iCombo) {
+            case InputCombo.ATTACK:
+                for (int i = 0; i < _cont; i++) {
+                    _attackArrows[i].sprite = _arrowFilled;
+                }
+                break;
+            case InputCombo.SPECIAL:
+                for (int i = 0; i < _cont; i++) {
+                    _specialArrows[i].sprite = _arrowFilled;
+                }
+                break;
+            case InputCombo.DEFENSE:
+                for (int i = 0; i < _cont; i++) {
+                    _defenseArrows[i].sprite = _arrowFilled;
+                }
+                break;
+        }
+    }
 }
