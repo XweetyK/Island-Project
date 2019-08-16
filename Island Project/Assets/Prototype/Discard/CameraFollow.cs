7666875bf99e06bb774ca5b1a4cad8e241be61fe
Leo <xwe>
2019-08-15 22:10:49 -0300

    Water Shader WIP

--- Island Project/Assets/Prototype/Discard/CameraFollow.cs
+++ Island Project/Assets/Prototype/Discard/CameraFollow.cs
@@ -6,6 +6,8 @@ public class CameraFollow : MonoBehaviour {
     [SerializeField] Transform _target;
     [SerializeField] float _speedX;
     [SerializeField] float _speedY;
+    [SerializeField] float _clampYMin;
+    [SerializeField] float _clampYMax;
     private Transform _childX;
     float _rotX;
     float _rotY;
@@ -22,7 +24,7 @@ public class CameraFollow : MonoBehaviour {
         if (Input.GetButton("Fire2")) {
             _rotX += _speedX * Input.GetAxis("Mouse Y") * Time.deltaTime;
             _rotY = _speedY * Input.GetAxis("Mouse X") * Time.deltaTime;
-            _rotX = Mathf.Clamp(_rotX, -10.0f, 40.0f);
+            _rotX = Mathf.Clamp(_rotX, _clampYMin, _clampYMax);
 
             _childX.transform.localRotation = Quaternion.Euler(-_rotX, 0, 0);
             gameObject.transform.Rotate(0.0f, _rotY, 0.0f);
