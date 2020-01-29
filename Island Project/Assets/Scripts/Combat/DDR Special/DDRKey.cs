a6d773e2810cdab04c299cd8feea880ed0d5b62a
Gaspar Nuñez <gaspacho09@hotmail.com>
2020-01-29 02:47:39 -0300

    DDR Start

--- Island Project/Assets/Scripts/Combat/DDR Special/DDRKey.cs
+++ Island Project/Assets/Scripts/Combat/DDR Special/DDRKey.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class DDRKey : MonoBehaviour
 {
-    public enum KeyTypes { DOWN, UP, LEFT, RIGHT };
-    void Start(){
-        
+    public enum KeyTypes { DOWN = 0, UP, LEFT, RIGHT };
+
+    KeyTypes _type;
+    float _speed;
+
+    private void Update()
+    {
+        transform.Translate(new Vector3(_speed, 0.0f));
     }
 
-    void Update(){
-        
+    public void setKey(KeyTypes type, float speed){
+        _type = type;
+        _speed = speed;
+        setSprite(_type);
+    }
+    void detonate(){
+        //stuff
+    }
+
+    void kill(){
+        //stuff
+    }
+    void setSprite(KeyTypes type){
+        //stuff
     }
 }
--- Island Project/Assets/Scripts/Combat/DDR Special/Key Spawner.cs
+++ Island Project/Assets/Scripts/Combat/DDR Special/Key Spawner.cs
@@ -5,19 +5,39 @@ using UnityEngine.UI;
 
 public class KeySpawner : MonoBehaviour
 {
-    [SerializeField] GameObject _SpritePrefab;
+    [SerializeField] GameObject _keyPrefab;
     [SerializeField] GameObject _EndZone;
     [SerializeField] GameObject _SpawnPoint;
 
-    [SerializeField] float initDelay = 1.0f;
-    [SerializeField] float keySpeed = 1.0f;
-    
-    void Start()
-    {
-        
-    }
+    [SerializeField] float _keySpeed = 1.0f;
+    [SerializeField] float _keyDelay = 2.0f;
+
+    bool _active = false;
+
     void Update()
     {
         
+    }
+    void startGame(float initDelay ,float keyDelay, float keySpeed)
+    {
+        _active = true;
+        StartCoroutine(DDRUpdate(initDelay,keyDelay,keySpeed));
+    }
+    IEnumerator DDRUpdate(float initDelay = 1.0f, float keyDelay = 1.0f, float keySpeed = 1.0f){
+        yield return new WaitForSeconds(initDelay);
+        while (_active)
+        {
+            GameObject go = Instantiate(_keyPrefab, _SpawnPoint.transform);
+            DDRKey.KeyTypes type = (DDRKey.KeyTypes)Random.Range(0,4);
+            go.GetComponent<DDRKey>().setKey(type, keySpeed);
+            yield return new WaitForSeconds(keyDelay);
+        }
+        yield return null;
+    }
+
+    public bool active
+    {
+        get { return _active; }
+        set { _active = value; }
     }
 }
