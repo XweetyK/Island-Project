1a8a8ae1d3ce4f00ae47c676a050645d259f6b55
Leo Perugini <xwe>
2020-02-03 07:42:57 -0300

    NPCS WIP
    dance fucker dance

---
+++ Island Project/Assets/Scripts/NPC/FollowUsername.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowUsername : MonoBehaviour 
+{ 
+    void Update()
+    {
+        transform.LookAt(GameObject.FindGameObjectWithTag("MainCamera").transform.position);
+    }
+}
---
+++ Island Project/Assets/Scripts/NPC/NpcBehavior.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NpcBehavior : MonoBehaviour
+{
+    public enum State {ROAMING, SITTING, IDLE, DANCE, CHEER, SING}
+    [SerializeField] private State _state;
+    [SerializeField] private bool _talking;
+    [SerializeField] private bool _fem;
+    private Animator _anim;
+    private NavMeshAgent _nma;
+
+    private void Start() {
+        _anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        UpdateAnim();
+    }
+    private void UpdateAnim() {
+        switch (_state) {
+            case State.ROAMING:
+                if (_nma.velocity.x > 0 && _nma.velocity.z > 0) {
+                    if (_fem) {
+                        _anim.SetBool("FemWalk", true);
+                    } else {
+                        _anim.SetBool("Walk", true);
+                    }
+                } else { _anim.SetBool("Walk", false); _anim.SetBool("FemWalk", false); }
+                break;
+            case State.SITTING:
+                if (_fem) {
+                    _anim.SetBool("SitFem", true);
+                } else {
+                    _anim.SetBool("SitMale", true);
+                }
+                break;
+            case State.IDLE:
+                if (_talking) {
+                    _anim.SetBool("Talk", true);
+                }
+                break;
+            case State.SING:
+                _anim.SetBool("Sing", true);
+                break;
+            case State.CHEER:
+                _anim.SetBool("Cheer", true);
+                break;
+            case State.DANCE:
+                _anim.SetBool("Dance", true);
+                break;
+
+        }
+    }
+}
