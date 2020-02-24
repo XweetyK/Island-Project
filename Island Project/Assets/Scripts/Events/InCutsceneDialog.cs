b585de3b89cabd9c65ed766050baea34d8faf7d5
Leo Perugini <xwe>
2020-02-24 04:42:22 -0300

    City WIP

---
+++ Island Project/Assets/Scripts/Events/InCutsceneDialog.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InCutsceneDialog : MonoBehaviour
+{
+    [SerializeField] Text _text;
+    [SerializeField] Text _shadow;
+    [SerializeField] float[] _timeStamps;
+    [SerializeField] string[] _dialogues;
+    int _count = 0;
+    float _time = 0;
+
+    void Start() {
+        DialogueCutscene();
+    }
+
+    private void Update() {
+        _time += Time.deltaTime;
+        if (_time >= _timeStamps[_count]) {
+            if (_count < _timeStamps.Length) {
+                DialogueCutscene();
+            }
+        }
+    }
+
+    void DialogueCutscene() {
+        _text.text = _dialogues[_count];
+        _shadow.text = _dialogues[_count];
+        _count++;
+    }
+}
